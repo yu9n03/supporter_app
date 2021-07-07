30e231c91f3ff0127645ca24538803bb302c85bd
yu9n03 <yu9n03@gmail.com>
2021-07-07 17:52:39 +0900

    userモデル単体テストの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   with_options presence:true do
     validates :nickname
     validates :age, format: { with: /\A\d{2}\z/} 
-    validates :height, format: { with: /\A(?:[1-2]{1}[0-9]{2})(?:\.[0-9]{1})?\z/}
-    validates :basis_weight, format: { with: /\A(?:[0-9]{2,3})(?:\.[0-9]{1,2})?\z/}
+    validates :height, format: { with: /\A([1-2]{1}[0-9]{2})(\.[0-9]{1})?\z/}
+    validates :basis_weight, format: { with: /\A([0-9]{2,3})(\.[0-9]{1,2})?\z/}
   end
 end
