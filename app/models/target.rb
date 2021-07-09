0474462891b8f106b0335255fb751212103be452
yu9n03 <yu9n03@gmail.com>
2021-07-09 14:55:49 +0900

    targetモデルにバリデーションを設定

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,10 @@
 class Target < ApplicationRecord
   belongs_to :user
   has_many :records
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :period_id
+
+  validates :goal_weight, presence: true, format: { with: /\A([0-9]{2,3})(\.[0-9]{1,2})?\z/ }
+  validates :period_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :plan, presence: true
 end
