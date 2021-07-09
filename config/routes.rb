ef41da75249d274fca952def02bd57c44b5f35f4
yu9n03 <yu9n03@gmail.com>
2021-07-09 15:21:03 +0900

    目標設定ページへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'records#index'
+  resources :records 
+  resources :targets, only: [:index, :new, :create, :show, :edit, :update]
 end
