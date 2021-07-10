8348903075ca466ef9613d577f2cc4303a2c9f5a
yu9n03 <yu9n03@gmail.com>
2021-07-10 17:41:10 +0900

    recordコントローラーにnew/createを定義

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,4 +3,22 @@ class RecordsController < ApplicationController
 
   def index
   end
+
+  def new
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:weight, :body_fat, :memo, :assessmet_id, :input_day).merge(user_id: current_user.id)
+  end
 end
