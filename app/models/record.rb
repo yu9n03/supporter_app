3f50523e970782248f5f2e4d2b007ff714fadaa0
yu9n03 <yu9n03@gmail.com>
2021-07-10 17:29:50 +0900

    recordモデルにバリデーションを設定

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,9 @@
 class Record < ApplicationRecord
   belongs_to :user
   belongs_to :target
+
+  validates :weight, presence: true, format: { with: /\A([0-9]{2,3})(\.[0-9]{1,2})?\z/ }
+  validates :body_fat, format: { with: /\A([0-9]{2})(\.[0-9]{1,2})?\z/ }
+  validates :assessment_id, presence: true, numericality: { true, message: "can't be blank" }
+  validates :input_day, presence: true
 end
