aa9bd5c469b37fdc0d3041cbc57ae799319d4abb
yu9n03 <yu9n03@gmail.com>
2021-07-11 14:18:31 +0900

    体重編集/削除機能を実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,24 +2,44 @@ class RecordsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @records = Record.where(user_id: current_user.id)
-  end
-
-  def new
+    @records = Record.where(user_id: current_user.id).limit(10).order('input_day DESC')
     @record = Record.new
   end
 
   def create
     @record = Record.new(record_params)
     if @record.save
-      redirect_to root_path
+      redirect_to action: :index
     else
-      render 'index'
+      @records = Record.where(user_id: current_user.id).limit(10).order('input_day DESC')
+      render :index
+    end
+  end
+
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    if @record.destroy
+      redirect_to action: :index
+    else
+      redirect_to action: :index
     end
   end
 
   private
   def record_params
-    params.permit(:weight, :body_fat, :memo, :assessment_id, :input_day).merge(user_id: current_user.id)
+    params.require(:record).permit(:weight, :body_fat, :memo, :assessment_id, :input_day).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'records#index'
-  resources :records 
+  resources :records,only: [:index, :create, :edit, :update, :destroy] 
   resources :users, only: :show 
   resources :targets, only: [:new, :create, :edit, :update]
 end
