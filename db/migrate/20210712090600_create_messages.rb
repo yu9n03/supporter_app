a0d43df1585804019f9f63f17aa10feeb42df280
yu9n03 <yu9n03@gmail.com>
2021-07-12 18:10:13 +0900

    テーブルの準備

--- db/migrate/20210712090600_create_messages.rb
+++ db/migrate/20210712090600_create_messages.rb
@@ -1,7 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-
+      t.text :text,       null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_053000) do
+ActiveRecord::Schema.define(version: 2021_07_12_090600) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.decimal "weight", precision: 5, scale: 2, null: false
@@ -50,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_07_09_053000) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "users"
   add_foreign_key "records", "users"
   add_foreign_key "targets", "users"
 end
