d7acf2fa5bfa221f7d96366358f72d0bcbd26bc6
yu9n03 <yu9n03@gmail.com>
2021-07-13 18:29:42 +0900

    チャット機能を非同期通信で実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,14 @@
 class MessagesController < ApplicationController
   
   def create
-    @message = Message.new(message_params)
-    @message.save
+    message = Message.new(message_params)
+    message.save
+    render json: {message: message}
   end
 
   private
   def message_params
     params.require(:message).permit(:text).merge(user_id: current_user.id)
   end
+
 end
---
+++ app/javascript/message.js
@@ -0,0 +1,43 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.message;
+  const html = `
+  <div class="message-set"> 
+    <div class="message-name">
+      ${item.user_id}
+    </div>
+    <div class="message-date">
+      ${item.created_at}
+    </div>
+  </div>
+  <div class="message">
+    ${item.text}
+  </div>`;
+  return html;
+};
+
+
+function post (){
+  const submit = document.getElementById("chat-submit-btn");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("message-form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/messages", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("message_text");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
+  });
+
+ };
+ 
+ window.addEventListener('load', post);
