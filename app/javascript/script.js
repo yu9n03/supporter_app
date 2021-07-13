e2cc18493464423798be31932c0ed6ee628f160c
yu9n03 <yu9n03@gmail.com>
2021-07-13 12:22:50 +0900

    モーダルを調整

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -2,7 +2,7 @@ $(function() {
   $('#chat-modal').click(function(){
     $('#show-chat-modal').fadeIn();
   });
-  $('#chat-submit-btn').click(function(){
-    $('#show-chat-modal').fadeIn();
+  $('#close-modal').click(function(){
+    $('#show-chat-modal').fadeOut();
   });
 });
