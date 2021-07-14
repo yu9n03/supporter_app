46b476b30bdf4cb935f13fa82dd013f85e025781
yu9n03 <yu9n03@gmail.com>
2021-07-14 15:50:09 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'records#index'
-  resources :records,only: [:index, :create, :edit, :update, :destroy] 
-  resources :users, only: :show 
-  resources :targets, only: [:new, :create, :edit, :update]
+  root to: 'rooms#index'
+  resources :records,only: [:create, :edit, :update, :destroy] 
+  resources :users, only:[:show] do
+    resources :rooms, only: :index
+  end
+  resources :targets, only: [:new, :create, :show, :edit, :update]
   resources :messages, only: :create
 end
