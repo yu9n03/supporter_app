8e71af55fb143f296ac90873520d6f8081176ec9
yu9n03 <yu9n03@gmail.com>
2021-07-16 15:29:15 +0900

    管理画面の調整

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,6 +7,16 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @record = Record.new
+    @records = Record.where(user_id: params[:id]).order('input_day DESC')
+    @message = Message.new
+    @messages = Message.where(user_id: params[:id]).order("created_at DESC")
+    @target = Target.find_by(user_id: params[:id])
+    @current_record = Record.where(user_id: params[:id]).limit(1).order('input_day DESC').last
+    @reserved = Reservation.where(user_id: params[:id]).limit(1).order('created_at DESC').last
+    if @target.present?
+      count_day
+    end
   end
 
   def destroy
@@ -19,5 +29,16 @@ class Admin::UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def count_day
+    @target = Target.find_by(user_id: params[:id])
+    @period = @target.period_id
+    @start_day = @target.created_at
+    @last_day = @start_day.since(@period.month)
+    @today = Time.now
+    
+    @remaining_second = (@last_day - @today).to_i
+    @remaining_day = ((@last_day - @today)/ 1.days).floor
   end
 end
