91cbeb80efcd4b308c924b26115b43e306c1a6fb
yu9n03 <yu9n03@gmail.com>
2021-07-16 10:08:24 +0900

    model作成とコントローラー作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,9 @@
+class Admin::UsersController < ApplicationController
+  before_action :if_not_admin
+  
+  
+  private
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_072054) do
+ActiveRecord::Schema.define(version: 2021_07_16_005130) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_072054) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "messages", "users"
