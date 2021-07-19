6901830914f2e1032a86b9c2db971eed86cce9ed
yu9n03 <yu9n03@gmail.com>
2021-07-19 18:34:41 +0900

    モデル単体テストコード（日本語化対応）とアソシエーションの修正

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,5 @@
 class Record < ApplicationRecord
   belongs_to :user
-  belongs_to :target, optional: true
 
   validates :weight, presence: true, format: { with: /\A([0-9]{2,3})(\.[0-9]{1,2})?\z/ }
   validates :assessment, presence: true
--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :record do
-    
+    weight                 { 88.25 }
+    body_fat              { 20.5 }
+    memo                  { Faker::Lorem.sentence }
+    input_day             { 2021-07-19 }
+    association :user
   end
 end
